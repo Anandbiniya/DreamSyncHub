e8c41cdc4cb7785d7b4a1afbd6ff6d88c1e5b3b6
Ayush <ayushphenomal1@gmail.com>
2023-09-22 17:01:49 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Card from "./components/Card";
+import Nav from "./components/Nav";
+import Hero from "./components/Hero"
+import Info from "./components/Info"
+import Ourteam from "./components/Ourteam"
+import Support from "./components/Support"
+import Footer from "./components/Footer"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+  <Nav/>
+  <Hero/>
+  <Info/>
+  <Card/>
+  <Ourteam/>
+  <Support/>
+  <Footer/>
+
+   </>
   );
 }
 
