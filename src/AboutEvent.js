6628f66afaf925be513dd2adf2e002b700ae0ce0
Ayush <ayushphenomal1@gmail.com>
2023-10-18 17:49:37 +0530

    added and updated event page

---
+++ src/AboutEvent.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import AEventHead from './components/AboutEvent/AEventHead'
+
+function AboutEvent() {
+  return (
+    <>
+    <div style={{ paddingTop: '66px' }}>
+
+    <AEventHead/>
+    </div>
+    </>
+  )
+}
+
+export default AboutEvent
--- src/components/eventinfocard.js
+++ src/components/eventinfocard.js
@@ -1,86 +1,89 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './Eventinfo.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBackward } from '@fortawesome/free-solid-svg-icons';
 import { faCircle } from '@fortawesome/free-solid-svg-icons';
-import { Modal, Button } from 'react-bootstrap';
+import { Button} from '@chakra-ui/react'
+import img_info from '../assets/photo1.png'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
-function EventInfoCard() {
-    const [show, setShow] = useState(true);
-
-    const handleClose = () =>{
-        setShow(false);
-        console.log("this is false")
-    } 
-    const handleShow = () =>{
-        setShow(true);
-        console.log('this is true')
-    
-    } 
-        
+function EventInfoCard() { 
 
     return (
         <>
-            <div className='event-info-div-outer'>
-                <div className='event-info-div' onClick={handleShow}>
-                    <div className='event-icon-div'>
-                        <div className='event-icon-inner'>
-                            <FontAwesomeIcon icon={faBackward} rotation={90} className='event-icon'/>
+            <div style={{ justifyContent: 'center', display:'flex' }}>            
+                <div className='event-info-div-outer'>
+                    <div className='event-info-div'>
+                        <div className='event-icon-div'>
+                            <div className='event-icon-inner'>
+                                <FontAwesomeIcon icon={faBackward} rotation={90} className='event-icon'/>
+                            </div>
+                        </div>
+                        <div className='upcoming-event'>
+                            <p className='event-text'>upcoming</p>
+                            <p>upcoming</p>
                         </div>
                     </div>
-                    <div className='upcoming-event'>
-                        <p>upcoming</p>
-                        <p>upcoming</p>
+                    
+                    <div className='event-info-div'>
+                        <div className='event-icon-div'>
+                            <div className='event-icon-inner'>
+                                <FontAwesomeIcon icon={faCircle} className='event-icon'/>
+                            </div>
+                        </div>
+                        <div className='upcoming-event'>
+                            <p className='event-text'>past event</p>
+                            <p>past event</p>
+                        </div>
                     </div>
                 </div>
-                
+            </div>
+            <div className='image-outer-div'>
+                <div className='image-topic'>
+                    <h2 className='image-topic-h'>
+                        Sneek peek
+                    </h2>
+                </div>
+                <div className='image-div'>
+                    <div className='image-left'>
+                        <div className='left-div'>
+                            <div className='left-inner'>
+                                <img className='left-img' src={img_info} alt='leftimg'/>
+                            </div>
+                        </div>
 
-                <Modal show={show} onClick={handleClose} className='outer-model'>
-                    <Modal.Header closeButton>
-                        <Modal.Title>Modal heading</Modal.Title>
-                    </Modal.Header>
-                    <Modal.Body className='modle-body'>
-                        <div class="popup cp-4">
-                        <div class="popup-items">
-                            <div class="popup-items-inner">
-                            <div class="popup-date"><h4>12</h4></div>
-                            <div class=" popup-month">12</div>
-                            <div class=" popup-time">2</div>
-                            <div class=" popup-venue ">venda,jhsdfbljhadbfk adfuag ue</div>
-                            <div className='joinus-btn'>                                
-                            <a href="https://chat.whatsapp.com/F16qdcRPSR8EevxlT75vYS" target="_blank" class="btn btn-primary">Join</a>
+                    </div>
+                    <div className='image-right-group'>
+                        <div className='img1-div'>
+                            <div className='img-group-1'>
+                                <img className='left-img' src={img_info} alt='leftimg'/>
                             </div>
+
+                        </div>
+                        <div className='img1-div'>
+                            <div className='img-group-1'>
+                                <img className='left-img' src={img_info} alt='leftimg'/>
                             </div>
-                        </div>
-                        <div class="col-md-8 d-flex flex-column align-self-center p-2">
-                            <h2 class="m-0">Event_Title</h2>
-                            <p>desc</p>
-                        </div>
-                        <div class="mt-3 d-flex justify-content-end align-self-center p-3" style={{ height: '150px' }}>
-                            <img src="" alt="Event Image" class="img-fluid h-100" />
-                        </div>
-                        </div>
-                    </Modal.Body>
-                <Modal.Footer>
-                    
-                    <Button variant="primary" onClick={handleClose}>
-                    Close
-                    </Button>
-                </Modal.Footer>
-                </Modal>
-                
-                <div className='event-info-div'>
-                    <div className='event-icon-div'>
-                        <div className='event-icon-inner'>
-                            <FontAwesomeIcon icon={faCircle} className='event-icon'/>
+
                         </div>
                     </div>
-                    <div className='upcoming-event'>
-                        <p>past event</p>
-                        <p>past event</p>
-                    </div>
+                </div>
+
+            </div>
+            <div className='d-flex flex-column justify-content-center align-items-center p-4'>
+                <div className='pb-5'>
+                    <h1>
+                        Join us
+                    </h1>
+                </div>
+                <div className='w-30'>
+                    <p className='text-center joinus-text'>
+                        Create butiful moments
+                    </p>
+                </div>
+                <div>
+                    <Button className='m-5' colorScheme='blue'>Button</Button>
                 </div>
             </div>
         </>
